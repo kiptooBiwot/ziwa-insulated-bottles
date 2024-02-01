1cedc2a9663b215570408c2bde6d048f2e8dc3f1
Elvis <elviskb@gmail.com>
2024-02-01 14:14:48 +0300

    discount message updated

--- components/DiscountStrip.vue
+++ components/DiscountStrip.vue
@@ -5,7 +5,9 @@
   <div class="bg-rose-500 w-full py-1">
     <div class="max-w-6xl mx-auto text-sm text-white font-semibold">
       <p class="text-center">
-        Valentine's Discount all products are 1,600/= until the 14th of February. Free delivery on orders over Ksh.6,000/=
+        <span class=" font-bold text-blue-100">Valentine's Discount</span> | All products are discounted until the 14th of
+        February.
+        Free delivery on orders over Ksh.6,000/=
       </p>
     </div>
   </div>
