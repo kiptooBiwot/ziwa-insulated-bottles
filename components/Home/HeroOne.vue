127ef50677cc1175fb9e5dd3ba527a7b55ac80a5
Elvis <elviskb@gmail.com>
2024-06-10 21:17:48 +0300

    updated the hero image

--- components/Home/HeroOne.vue
+++ components/Home/HeroOne.vue
@@ -1,16 +1,23 @@
 <script setup></script>
 
 <template>
-  <div class="w-full h-auto lg:h-[500px]">
+  <div class="w-full h-auto lg:h-[500px] relative">
+    <div class="absolute inset-0 bg-[#98BEFC]"></div>
     <!-- <div class="absolute inset-0 max-w-7xl mx-auto">
       <h2 class="absolute top-10 text-white font-bold text-6xl text-center">
         Hydration Meets Insulation
       </h2>
     </div> -->
-    <img
+    <!-- src="/images/ziwa_hero.jpeg" -->
+    <!-- <img
       src="/images/ziwa_hero.jpeg"
       alt=""
-      class="h-[250px] lg:h-[500px] w-full object-cover object-bottom"
+      class="h-[250px] lg:h-[500px] w-full object-contain object-bottom"
+    /> -->
+    <img
+      src="/images/fathers-day-discount.jpeg"
+      alt=""
+      class="h-[250px] lg:h-[500px] w-full object-contain object-bottom absolute"
     />
   </div>
 </template>
