0ff187a8b9fc800eac1fd0821e117e5cdbd37f8b
Elvis <elviskb@gmail.com>
2024-06-26 21:48:06 +0300

    hero image bug fixed

--- components/Home/HeroOne.vue
+++ components/Home/HeroOne.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="w-full h-auto lg:h-[500px] relative">
-    <div class="hidden md:flex lg:absolute lg:inset-0 lg:bg-[#98BEFC]"></div>
+    <div class="hidden md:flex lg:absolute lg:inset-0 lg:bg-[#98BEFC"></div>
     <!-- <div class="absolute inset-0 max-w-7xl mx-auto">
       <h2 class="absolute top-10 text-white font-bold text-6xl text-center">
         Hydration Meets Insulation
@@ -12,7 +12,7 @@
     <img
       src="/images/ziwa_hero.jpeg"
       alt=""
-      class="h-[250px] lg:h-[500px] w-full object-contain object-bottom"
+      class="h-[250px] lg:h-[500px] w-full object-cover lg:object-fill object-bottom"
     />
     <!-- <img
       src="/images/fathers-day-discount.jpeg"
