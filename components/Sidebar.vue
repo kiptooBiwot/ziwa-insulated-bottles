047caa09be7c49befa3c08d3a5a4c7a731dd5510
Elvis <elviskb@gmail.com>
2024-08-30 14:52:33 +0300

    replaced a few tumbler images

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -31,7 +31,7 @@ const showDropdown = ref(false)
           ? 'translate-x-0 ease-out'
           : '-translate-x-full ease-in'
       "
-      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0"
+      class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 md:translate-x-0 lg:static lg:inset-0"
     >
       <div class="flex items-center justify-center mt-8">
         <div class="flex items-center">
